b9216ad46060620b34bb5f70585c8c51cdfb007e
sherryh <enjoy.wind7rain@gmail.com>
2023-05-27 12:31:17 +0200

    Fixed vtt import linting issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-})
+});
 
 module.exports = withPWA({
-  // config
-})
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|vtt)$/,
+      type: 'asset',
+      generator: {
+        filename: 'static/chunks/[path][name].[hash][ext]',
+      },
+    });
+
+    return config;
+  },
+});
